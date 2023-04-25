c0d00540bce133d17aaa52e362a57fbfa1adc3b6
naji-alhusami <najihussami@gmail.com>
2023-04-25 13:03:12 +0300

    test useRouter

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,10 +1,16 @@
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
-    return (
-      <div>
-        <h1>The Portfolio Project Page</h1>
-      </div>
-    );
-  }
-  
-  export default PortfolioProjectPage;
-  
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  );
+}
+
+export default PortfolioProjectPage;
