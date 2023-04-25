79dc1c29f53e2c44f93606ff9f262443e049c3ad
naji-alhusami <najihussami@gmail.com>
2023-04-25 13:30:38 +0300

    test catch all routes using ...

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,10 @@
+function ClientProjectId() {
+    return (
+      <div>
+        <h1>The ID of a given project of a client</h1>
+      </div>
+    );
+  }
+  
+  export default ClientProjectId;
+  
