841be3ae18d8ac60dd6c1c93fe531f90dc35ee9c
naji-alhusami <najihussami@gmail.com>
2023-04-30 13:53:10 +0300

    added each event alone

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,34 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
-    return (
-      <div>
-        <h1>Event Detail</h1>
-      </div>
-    );
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No Event Found!</p>;
   }
-  
-  export default EventDetailPage;
-  
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        image={event.image}
+        address={event.location}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+}
+
+export default EventDetailPage;
