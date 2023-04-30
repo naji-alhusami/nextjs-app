691cae3d9f7857639b0f25541453ca6c3a657acb
naji-alhusami <najihussami@gmail.com>
2023-04-30 15:11:01 +0300

    added filteredEvents cases

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,10 +1,47 @@
+import { useRouter } from "next/router";
+
+import { getFilteredEvents } from "../../dummy-data";
+
 function FilteredEventsPage() {
-    return (
-      <div>
-        <h1>Filtered Events</h1>
-      </div>
-    );
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
   }
-  
-  export default FilteredEventsPage;
-  
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p> Invalid Filter, Please adjust your values</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No Evevnts found for the chosen filter!</p>;
+  }
+
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
+}
+
+export default FilteredEventsPage;
