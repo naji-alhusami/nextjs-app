db5b1ebc468913b79cc20eae0f32a698c3c42749
naji-alhusami <najihussami@gmail.com>
2023-04-30 13:15:00 +0300

    added styles

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+import Button from "../ui/button";
+import classes from "./event-item.module.css";
+
 function EventItem(props) {
   const { id, title, image, date, location } = props;
 
@@ -14,19 +17,19 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{readableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
